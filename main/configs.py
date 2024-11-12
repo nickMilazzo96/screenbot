39a314b8a955233c9e232b732161408244c8d229
nickMilazzo96 <163031330+nickMilazzo96@users.noreply.github.com>
2024-11-12 11:34:39 -0500

    Update configs.py

--- main/configs.py
+++ main/configs.py
@@ -1,10 +1,8 @@
 ### OpenAI API key ###
-api_key = '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'
 model_id = 'ft:gpt-4o-mini-2024-07-18:personal::AG5eHVCS' # model tuned with v3 data on Oct 8
 ###
 
 ### Airtable Configs ###
-AIRTABLE_API_KEY = 'patnVP34xrFD8xPS9.37b8c996a32aab3600d686b4c20c848236c5a18de769a7031599e2dcbe3a7467'
 BASE_ID = 'app71xlZgKi5nUTQy'  # first part of url, beginning with "app"
 TABLE_NAME = 'tbl6quzHPHYGgIDXW' # second part of url, beginning with "tbl"
 
